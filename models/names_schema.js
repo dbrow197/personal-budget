e0401ecd67bcb25f15f36e6b4ed92207452b1070
Dan Brown <dbrow197@uncc.edu>
2020-10-30 17:57:32 -0400

    labels work

--- models/names_schema.js
+++ models/names_schema.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const nameSchema = new mongoose.Schema({
     title: {
         type: String,
+        trim: true,
         required: true,
         maxLength: 25
     },
--- server.js
+++ server.js
@@ -1,59 +1,55 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const budgetModel = require("./models/names_schema")
 const app = express();
 const port = 5000;
 const url = 'mongodb://localhost:27017/personal_budget';
 
-//app.use('/', express.static('public'));
-
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-})
-.then(() => {
-  console.log("Connected to the database")
-  budgetModel.find({})
-  .then((data) => {console.log(data)
-  })
-})
-
-let Budget_listSchema = new mongoose.Schema({
-  title: {
-      type: String,
-      required: true,
-      maxLength: 25
-  },
-  cost: {
-      type: Number,
-      required: true,
-      min: 1.00
-  },
-  color: {
-      type: String,
-      required: true,
-      validator: [(hexColor) => (/^#[0-9A-F]{6}$/i).test(hexColor), 'Invalid hex color value']
-  }
-});
-
-let Budget_list = mongoose.model('Budget_list', Budget_listSchema);
-
 app.use('/', express.static('public'));
 app.use(express.json());
 
-app.get('/budget_lists', (req, res) => {
-  Budget_list.find({}, (error, budget_lists) => {
-        res.send(error || budget_lists);
-    });
+
+
+
+
+app.get('/personal_budget', (req, res) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    budgetModel.find({})
+                .then((data) => {
+                    res.json(data);
+                    mongoose.connection.close();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
-app.post('/budget_lists', (req, res) => {
-  Budget_list.create(req.body, (error, budget_list) => {
-        res.send(error || budget_list);
-    })
+app.post('/personal_budget', (req, res) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    budgetModel.find({})
+                .then((data) => {
+                    res.json(data);
+                    mongoose.connection.close();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 app.listen(port, () => {
